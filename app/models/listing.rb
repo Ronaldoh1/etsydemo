6d6f538ca63056d53c16aaae73b2455a692f3e77
Ronald Hernandez <2ronald.hernandez@gmail.com>
2014-10-18 02:49:52 -0400

    linked listings to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,4 +12,6 @@ has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" 
 	validates :price, numericality: {greater_than: 0}
 	validates_attachment_presence :image
 
+	belongs_to :user
+
 end
